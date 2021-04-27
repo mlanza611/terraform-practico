2fbd52a864f83e6e3a8503736a68fd8776920849
mlanza611 <manu_lanza98@hotmail.com>
2021-04-27 08:19:47 -0300

    'Terraform'

--- main.tf
+++ main.tf
@@ -1,13 +1,9 @@
-provider "aws" {
-  region  = "us-east-1"
-  profile = "default"
-}
 resource "aws_instance" "test-terraform-ec2" {
-  ami           = "ami-06e2b3882a1e987b7"
-  instance_type = "t2.micro"
+  ami           = var.centos7
+  instance_type = var.instance_type
   key_name      = "key-pair-ssh"
   vpc_security_group_ids = [aws_security_group.test-terraform-sg.id]
-  subnet_id = aws_subnet_test.terraform-subnet.id
+  subnet_id = aws_subnet.test-terraform-subnet.id
   tags = {
     Name      ="test-terraform-ec2"
     terraform ="True"
